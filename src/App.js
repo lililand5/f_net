7b86c1935fae2b15771d7d615e1eca58b36c4e49
liliya-tagirova <l.tagirova@edstein.ru>
2023-08-27 11:55:47 +0300

    manage

--- src/App.js
+++ src/App.js
@@ -1,37 +1,50 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Maincontent from "./components/Maincontent";
-import Leftsidebar from "./components/Leftsidebar";
-import Rightsidebar from "./components/Rightsidebar";
-import SignUp from "./components/SignUp";
-import Login from "./components/Login";
-import Subscriptions from "./components/Subscriptions";
-import Followers from "./components/Followers";
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Maincontent from "./components/Maincontent";
+// import Leftsidebar from "./components/Leftsidebar";
+// import Rightsidebar from "./components/Rightsidebar";
+// import SignUp from "./components/SignUp";
+// import Login from "./components/Login";
+// import Subscriptions from "./components/Subscriptions";
+// import Followers from "./components/Followers";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<MainLayout />} />
+//         <Route path="/signup" element={<SignUp />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/subscriptions" element={<Subscriptions />} />
+//         <Route path="/followers" element={<Followers />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// function MainLayout() {
+//   return (
+//     <div className="container-fluid gedf-wrapper">
+//       <div className="row">
+//         <Leftsidebar />
+//         <Maincontent />
+//         <Rightsidebar />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import HelloComponent from './HelloComponent';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<MainLayout />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/subscriptions" element={<Subscriptions />} />
-        <Route path="/followers" element={<Followers />} />
-      </Routes>
-    </Router>
-  );
-}
-
-function MainLayout() {
-  return (
-    <div className="container-fluid gedf-wrapper">
-      <div className="row">
-        <Leftsidebar />
-        <Maincontent />
-        <Rightsidebar />
-      </div>
+    <div className="App">
+      <HelloComponent />
     </div>
   );
 }
